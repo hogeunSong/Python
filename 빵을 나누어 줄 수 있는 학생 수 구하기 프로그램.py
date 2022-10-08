d174d97251d83aeca2c07a2bdf6849f9c8e8b7aa
송호근 <115344824+hogeunSong@users.noreply.github.com>
2022-10-08 23:12:57 +0900

    Add files via upload

---
+++ 빵을 나누어 줄 수 있는 학생 수 구하기 프로그램.py
@@ -0,0 +1,15 @@
+bread = 97
+count = 3
+maxCount = bread // count
+print('빵을 나누어 줄 수 있는 학생 수 : ', maxCount)
+rest = bread % count
+print('남은 빵 개수 : ', rest)
+
+print('-------------------------------------------------------')
+
+strawberry_bread = int(input('딸기빵의 갯수: '))
+counting = int(input('1인당 돌아가는 딸기빵의 개수: ' ))
+maxCounting = strawberry_bread // counting
+print('딸기빵을 나누어 줄 수 있는 학생 수 : ', maxCounting)
+rests = strawberry_bread % counting
+print('남은 딸기빵의 개수 : ', rests)
