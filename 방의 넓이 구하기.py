6af6ab294703cfdb3341b182b2d0087d9c6157d3
송호근 <115344824+hogeunSong@users.noreply.github.com>
2022-10-08 22:19:18 +0900

    Add files via upload

---
+++ 방의 넓이 구하기.py
@@ -0,0 +1,11 @@
+width = int(input('가로 길이 : '))
+length = int(input('세로 길이 : '))
+area = width * length
+print('방의 넓이:', area, 'cm')
+
+print('--------------------------------------------------')
+
+w = int(input('가로의 길이 ?'))
+h = int(input('세로의 길이 ?'))
+room_area = w * h
+print('방의 넓이:', room_area,'cm')
